10f7ad17ae79210c17c7e3c17e6b1946b812595b
Andre Barsotti <andre.barsotti@accelera.com.br>
2021-06-10 20:41:09 -0300

    Xamarim Cli

---
+++ src/NotificationHubCli/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace NotificationHubCli
+{
+    public static class Constants
+    {
+        public const string NotificationHubName = "comgas-dev-hub";
+        public const string ListenConnectionString = "Endpoint=sb://comgas-dev-nh.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=UdWZFKzTvwtKwgVVLlbSayVCNMl9GJDHPQDoSaZXxuo=";
+    }
+}
--- src/NotificationHubCli/MainActivity.cs
+++ src/NotificationHubCli/MainActivity.cs
@@ -2,18 +2,28 @@
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
+using WindowsAzure.Messaging.NotificationHubs;
 
 namespace NotificationHubCli
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            // Listen for push notifications
+            NotificationHub.SetListener(new AzureListener());
+
+            // Start the SDK
+            NotificationHub.Start(Application, Constants.NotificationHubName, Constants.ListenConnectionString);
+
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
